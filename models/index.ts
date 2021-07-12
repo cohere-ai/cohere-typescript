2121e1e7153c37f98b5ab7389388c08684790083
Robert Kozin <robert@robertkozin.com>
2021-07-12 12:28:28 -0400

    Add `tokens` and `token_log_likelihoods` to ChooseBest response

--- models/index.ts
+++ models/index.ts
@@ -54,7 +54,9 @@ export interface embeddings {
 }
 
 export interface likelihoods {
-  likelihoods: number[]
+  likelihoods: number[],
+  tokens: string[][],
+  token_log_likelihoods: number[][],
   [key: string]: any,
 }
 
