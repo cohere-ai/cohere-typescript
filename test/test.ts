c40802f9591bca0bdb9c115351a4f069858387e1
Michael Kozakov <michael.kozakov@gmail.com>
2022-11-21 14:21:40 -0500

    Remove extract

--- test/test.ts
+++ test/test.ts
@@ -8,6 +8,5 @@ describe("The `small` model", () => {
   importTest("generate", "./generate.ts");
   importTest("embed", "./embed.ts");
   importTest("classify", "./classify.ts");
-  importTest("extract", "./extract.ts");
   importTest("tokenize", "./tokenize.ts");
 });
