a0ab3552a95ebe5fb50bcf089ad9beb1375539da
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2023-10-30 14:25:03 +0000

    SDK regeneration

---
+++ src/serialization/client/requests/index.ts
@@ -0,0 +1,9 @@
+export { GenerateRequest } from "./GenerateRequest";
+export { EmbedRequest } from "./EmbedRequest";
+export { ClassifyRequest } from "./ClassifyRequest";
+export { ChatRequest } from "./ChatRequest";
+export { TokenizeRequest } from "./TokenizeRequest";
+export { DetokenizeRequest } from "./DetokenizeRequest";
+export { DetectLanguageRequest } from "./DetectLanguageRequest";
+export { SummarizeRequest } from "./SummarizeRequest";
+export { RerankRequest } from "./RerankRequest";
