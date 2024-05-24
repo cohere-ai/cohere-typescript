1f8c9b756a68510e95ad846cdfad1999323e6cee
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-05-24 07:42:43 +0000

    SDK regeneration

--- src/core/streaming-fetcher/Stream.ts
+++ src/core/streaming-fetcher/Stream.ts
@@ -1,35 +1,93 @@
 import { Readable } from "stream";
-import { SuccessfulResponse } from "../fetcher/APIResponse";
-import { StreamUtils } from "./streaming-utils";
+import { RUNTIME } from "../runtime";
+
+export declare namespace Stream {
+    interface Args {
+        /**
+         * The HTTP response stream to read from.
+         */
+        stream: Readable | ReadableStream;
+        /**
+         * The event shape to use for parsing the stream data.
+         */
+        eventShape: JsonEvent | SseEvent;
+    }
+
+    interface JsonEvent {
+        type: "json";
+        messageTerminator: string;
+    }
+
+    interface SseEvent {
+        type: "sse";
+        streamTerminator?: string;
+    }
+}
+
+const DATA_PREFIX = "data:";
 
 export class Stream<T> implements AsyncIterable<T> {
-    private response: SuccessfulResponse<Readable>;
+    private stream: Readable | ReadableStream;
     private parse: (val: unknown) => Promise<T>;
-    private terminator: string;
+    /**
+     * The prefix to use for each message. For example,
+     * for SSE, the prefix is "data: ".
+     */
+    private prefix: string | undefined;
+    private messageTerminator: string;
+    private streamTerminator: string | undefined;
 
-    constructor({
-        response,
-        parse,
-        terminator,
-    }: {
-        response: SuccessfulResponse<Readable>;
-        parse: (val: unknown) => Promise<T>;
-        terminator: string;
-    }) {
-        this.response = response;
+    constructor({ stream, parse, eventShape }: Stream.Args & { parse: (val: unknown) => Promise<T> }) {
+        this.stream = stream;
         this.parse = parse;
-        this.terminator = terminator;
+        if (eventShape.type === "sse") {
+            this.prefix = DATA_PREFIX;
+            this.messageTerminator = "\n";
+            this.streamTerminator = eventShape.streamTerminator;
+        } else {
+            this.messageTerminator = eventShape.messageTerminator;
+        }
     }
 
     private async *iterMessages(): AsyncGenerator<T, void> {
-        const sse = Boolean(this.response?.headers?.get("content-type")?.includes("text/event-stream"));
-        const stream: StreamUtils<{}> = sse ?
-            StreamUtils.fromSSEResponse(this.response)
-            : StreamUtils.fromReadableStream(this.response.body);
+        const stream = readableStreamAsyncIterable<any>(this.stream);
+        let buf = "";
+        let prefixSeen = false;
+        for await (const chunk of stream) {
+            buf += this.decodeChunk(chunk);
 
-        for await (const obj of stream) {
-            const message = await this.parse(obj);
-            yield message;
+            let terminatorIndex: number;
+            // Parse the chunk into as many messages as possible
+            while ((terminatorIndex = buf.indexOf(this.messageTerminator)) >= 0) {
+                // Extract the line from the buffer
+                let line = buf.slice(0, terminatorIndex + 1);
+                buf = buf.slice(terminatorIndex + 1);
+
+                // Skip empty lines
+                if (line.length === 0) {
+                    continue;
+                }
+
+                // Skip the chunk until the prefix is found
+                if (!prefixSeen && this.prefix != null) {
+                    const prefixIndex = line.indexOf(this.prefix);
+                    if (prefixIndex === -1) {
+                        continue;
+                    }
+                    prefixSeen = true;
+                    line = line.slice(prefixIndex + this.prefix.length);
+                }
+
+                // If the stream terminator is present, return
+                if (this.streamTerminator != null && line.includes(this.streamTerminator)) {
+                    return;
+                }
+
+                // Otherwise, yield message from the prefix to the terminator
+                const message = await this.parse(JSON.parse(line));
+                yield message;
+                prefixSeen = false;
+            }
         }
     }
 
@@ -38,4 +96,53 @@ export class Stream<T> implements AsyncIterable<T> {
             yield message;
         }
     }
+
+    private decodeChunk(chunk: any): string {
+        let decoded = "";
+        // Buffer is present in Node.js environment
+        if (RUNTIME.type === "node" && typeof chunk != "undefined") {
+            decoded += Buffer.isBuffer(chunk) ? chunk : Buffer.from(chunk);
+        }
+        // TextDecoder is present in Browser environment
+        else if (RUNTIME.type === "browser" && typeof TextDecoder !== "undefined") {
+            const decoder = new TextDecoder("utf8");
+            decoded += decoder.decode(chunk);
+        }
+        return decoded;
+    }
+}
+
+/**
+ * Browser polyfill for ReadableStream
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function readableStreamAsyncIterable<T>(stream: any): AsyncIterableIterator<T> {
+    if (stream[Symbol.asyncIterator]) {
+        return stream;
+    }
+
+    const reader = stream.getReader();
+    return {
+        async next() {
+            try {
+                const result = await reader.read();
+                if (result?.done) {
+                    reader.releaseLock();
+                } // release lock when stream becomes closed
+                return result;
+            } catch (e) {
+                reader.releaseLock(); // release lock when stream becomes errored
+                throw e;
+            }
+        },
+        async return() {
+            const cancelPromise = reader.cancel();
+            reader.releaseLock();
+            await cancelPromise;
+            return { done: true, value: undefined };
+        },
+        [Symbol.asyncIterator]() {
+            return this;
+        },
+    };
 }
