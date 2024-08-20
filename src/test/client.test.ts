3068c785f30b825c94debda8938890639b3a1547
billytrend-cohere <144115527+billytrend-cohere@users.noreply.github.com>
2024-08-20 06:29:35 -0500

    Fix v1 environment issue (#196)

---
+++ src/test/client.test.ts
@@ -0,0 +1,29 @@
+import { describe, test } from "@jest/globals";
+import { CohereClient } from "../index";
+
+describe("v1 back compat", () => {
+    test.each([
+        "https://api.cohere.com/",
+        "https://api.cohere.com/v1",
+        "https://api.cohere.com/v1/",
+        "https://api.cohere.com/v1//",
+        "https://api.cohere.com/v2",
+    ])("%s", async (environment) => {
+        let url = ""
+
+        const cohere = new CohereClient({
+            token: "token",
+            environment,
+            fetcher: (async (opts) => {
+                url = opts.url
+                throw "we're done"
+            })
+        })
+
+        try {
+            await cohere.chat({ message: "hello" })
+        } catch { }
+
+        expect(url).toMatchSnapshot()
+    })
+});
