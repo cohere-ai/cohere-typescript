fe9d0fc123978db5e9f685a59b66cdef8b01c541
billytrend-cohere <144115527+billytrend-cohere@users.noreply.github.com>
2024-11-28 11:42:59 +0000

    Fix break

--- src/test/cloud-tests/rerank-2.test.ts
+++ src/test/cloud-tests/rerank-2.test.ts
@@ -5,16 +5,10 @@ skipIfTestTypeIsnt("rerank-2")("rerank-2", () => {
     test.concurrent("works", async () => {
         const rerank = await cohere.v2.rerank({
             documents: [
-                { text: "Carson City is the capital city of the American state of Nevada." },
-                {
-                    text: "The Commonwealth of the Northern Mariana Islands is a group of islands in the Pacific Ocean. Its capital is Saipan.",
-                },
-                {
-                    text: "Washington, D.C. (also known as simply Washington or D.C., and officially as the District of Columbia) is the capital of the United States. It is a federal district.",
-                },
-                {
-                    text: "Capital punishment (the death penalty) has existed in the United States since beforethe United States was a country. As of 2017, capital punishment is legal in 30 of the 50 states.",
-                },
+                "Carson City is the capital city of the American state of Nevada.",
+                "The Commonwealth of the Northern Mariana Islands is a group of islands in the Pacific Ocean. Its capital is Saipan.",
+                "Washington, D.C. (also known as simply Washington or D.C., and officially as the District of Columbia) is the capital of the United States. It is a federal district.",
+                "Capital punishment (the death penalty) has existed in the United States since beforethe United States was a country. As of 2017, capital punishment is legal in 30 of the 50 states."
             ],
             query: "What is the capital of the United States?",
             topN: 3,
