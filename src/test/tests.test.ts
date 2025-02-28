504683bb7f3a7ee1d663661232f022649d545982
Billy Trend <billy@cohere.com>
2025-02-28 16:26:28 -0500

    Fixes tests

--- src/test/tests.test.ts
+++ src/test/tests.test.ts
@@ -99,24 +99,6 @@ describe("test sdk", () => {
         }
     });
 
-    test.concurrent("classify works", async () => {
-        const classify = await cohere.classify({
-            examples: [
-                { text: "Dermatologists don't like her!", label: "Spam" },
-                { text: "'Hello, open to this?'", label: "Spam" },
-                { text: "I need help please wire me $1000 right now", label: "Spam" },
-                { text: "Nice to know you ;)", label: "Spam" },
-                { text: "Please help me?", label: "Spam" },
-                { text: "Your parcel will be delivered today", label: "Not spam" },
-                { text: "Review changes to our Terms and Conditions", label: "Not spam" },
-                { text: "Weekly sync notes", label: "Not spam" },
-                { text: "'Re: Follow up from today's meeting'", label: "Not spam" },
-                { text: "Pre-read for tomorrow", label: "Not spam" },
-            ],
-            inputs: ["Confirm your email address", "hey i need u to send some $"],
-        });
-    });
-
     test.concurrent("tokenize works", async () => {
         const tokenize = await cohere.tokenize({
             text: "tokenize me! :D",
@@ -249,6 +231,7 @@ describe("test sdk", () => {
                 "date": "2023-09-29",
                 "id": "sales_database:0:0",
                 "numberOfSales": "120",
+                "tool_name": "sales_database",
                 "totalRevenue": "48500",
               },
             ]
