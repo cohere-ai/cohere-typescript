228434b0cb53616f019dfebeb040e5d7e07d58ef
billytrend-cohere <144115527+billytrend-cohere@users.noreply.github.com>
2025-04-16 17:12:53 +0100

    Add V2 AWS clients (#224)
    * Add AwsClientV2 and corresponding BedrockClientV2 and SagemakerClientV2 classes
    
    * Fix path

--- src/BedrockClient.ts
+++ src/BedrockClient.ts
@@ -1,8 +1,14 @@
 import { AwsProps, fetchOverride } from './aws-utils';
-import { AwsClient } from './AwsClient';
+import { AwsClient, AwsClientV2 } from './AwsClient';
 import { CohereClient } from "./Client";
 
 export class BedrockClient extends AwsClient {
+    constructor(_options: CohereClient.Options & AwsProps) {
+        super({ ..._options, fetcher: fetchOverride("bedrock", _options) });
+    }
+}
+
+export class BedrockClientV2 extends AwsClientV2 {
     constructor(_options: CohereClient.Options & AwsProps) {
         super({ ..._options, fetcher: fetchOverride("bedrock", _options) });
     }
