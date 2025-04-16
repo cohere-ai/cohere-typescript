495d200158a4ef97200f94b1bb4ce616f9f161ef
Billy Trend <billy@cohere.com>
2025-04-16 15:34:48 +0100

    Add AwsClientV2 and corresponding BedrockClientV2 and SagemakerClientV2 classes

--- src/AwsClient.ts
+++ src/AwsClient.ts
@@ -1,7 +1,15 @@
+import { CohereClientV2 } from 'ClientV2';
 import { AwsProps } from './aws-utils';
 import { CohereClient } from "./Client";
 
 export class AwsClient extends CohereClient {
+    constructor(_options: CohereClient.Options & AwsProps) {
+        _options.token = "n/a"; // AWS clients don't need a token but setting to this to a string so Fern doesn't complain
+        super(_options);
+    }
+}
+
+export class AwsClientV2 extends CohereClientV2 {
     constructor(_options: CohereClient.Options & AwsProps) {
         _options.token = "n/a"; // AWS clients don't need a token but setting to this to a string so Fern doesn't complain
         super(_options);
--- src/SagemakerClient.ts
+++ src/SagemakerClient.ts
@@ -1,9 +1,15 @@
-import { AwsClient } from './AwsClient';
+import { AwsClient, AwsClientV2 } from './AwsClient';
 import { CohereClient } from "./Client";
 import { AwsProps, fetchOverride } from './aws-utils';
 
 
 export class SagemakerClient extends AwsClient {
+    constructor(_options: CohereClient.Options & AwsProps) {
+        super({ ..._options, fetcher: fetchOverride("sagemaker", _options) });
+    }
+}
+
+export class SagemakerClientV2 extends AwsClientV2 {
     constructor(_options: CohereClient.Options & AwsProps) {
         super({ ..._options, fetcher: fetchOverride("sagemaker", _options) });
     }
