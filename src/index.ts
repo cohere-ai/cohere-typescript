3d98d6c5af3b5dde41a98f9aa3e02951af61df0a
Billy Trend <billy@cohere.com>
2025-04-16 17:19:04 +0100

    Export

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export * as Cohere from "./api";
-export { BedrockClient } from "./BedrockClient";
+export { BedrockClient, BedrockClientV2 } from "./BedrockClient";
 export { CohereClientV2 } from "./ClientV2";
 export { CustomClient as CohereClient } from "./CustomClient";
 export { CohereEnvironment } from "./environments";
 export { CohereError, CohereTimeoutError } from "./errors";
-export { SagemakerClient } from "./SagemakerClient";
+export { SagemakerClient, SagemakerClientV2 } from "./SagemakerClient";
 
