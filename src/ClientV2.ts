7f06795d95530a044a08adde66277721548b8441
fern-support <jon@buildwithfern.com>
2026-03-26 11:01:42 -0400

    Changed fernignore files

--- src/ClientV2.ts
+++ src/ClientV2.ts
@@ -1,18 +1,18 @@
-import { V2 } from "./api/resources/v2/client/Client";
+import { V2Client } from "./api/resources/v2/client/Client";
 import { CohereClient } from "./Client";
 
 // this class will require manual updates over time
-export class CohereClientV2 implements Omit<CohereClient, keyof V2 | "v2">, Pick<V2, keyof V2> {
+export class CohereClientV2 implements Omit<CohereClient, keyof V2Client | "v2">, Pick<V2Client, keyof V2Client> {
     constructor(private _options: CohereClient.Options) {
     }
 
     private client = new CohereClient(this._options);
-    private clientV2 = new V2(this._options);
+    private clientV2 = new V2Client(this._options);
 
-    chat: typeof V2.prototype.chat = this.clientV2.chat.bind(this.clientV2)
-    chatStream: typeof V2.prototype.chatStream = this.clientV2.chatStream.bind(this.clientV2)
-    embed: typeof V2.prototype.embed = this.clientV2.embed.bind(this.clientV2)
-    rerank: typeof V2.prototype.rerank = this.clientV2.rerank.bind(this.clientV2)
+    chat: typeof V2Client.prototype.chat = this.clientV2.chat.bind(this.clientV2)
+    chatStream: typeof V2Client.prototype.chatStream = this.clientV2.chatStream.bind(this.clientV2)
+    embed: typeof V2Client.prototype.embed = this.clientV2.embed.bind(this.clientV2)
+    rerank: typeof V2Client.prototype.rerank = this.clientV2.rerank.bind(this.clientV2)
 
     generateStream: typeof CohereClient.prototype.generateStream = this.client.generateStream.bind(this.clientV2)
     generate: typeof CohereClient.prototype.generate = this.client.generate.bind(this.clientV2)
@@ -28,4 +28,5 @@ export class CohereClientV2 implements Omit<CohereClient, keyof V2 | "v2">, Pick
     models: typeof CohereClient.prototype.models = this.client.models
     finetuning: typeof CohereClient.prototype.finetuning = this.client.finetuning
     batches: typeof CohereClient.prototype.batches = this.client.batches
+    audio: typeof CohereClient.prototype.audio = this.client.audio
 }
